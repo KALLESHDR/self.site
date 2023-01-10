e66951e7350758229d01d7e6e6488139182e7570
Kallesh D R <90609484+KALLESHDR@users.noreply.github.com>
2023-01-11 00:56:28 +0530

    HTML

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Kallesh D R</span> "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
